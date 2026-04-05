3d4c55e748fe7af4fb73f2a10c034523fc846f9f
Ritarup Tripathy <71173386+Ritarup@users.noreply.github.com>
2026-04-06 03:57:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const items = document.querySelectorAll(".faq-item");
+
+items.forEach(item => {
+  item.addEventListener("click", () => {
+
+    
+    items.forEach(i => {
+      if(i !== item) i.classList.remove("active");
+    });
+
+    item.classList.toggle("active");
+  });
+});
